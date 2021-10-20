c5c154928ea78ec46f1dee296a2748fcdb4be6a1
GyeomFka <clazziquai01@gmail.com>
2021-10-20 10:19:28 +0900

    chore: rename DESC.md for 1강.md

---
+++ README.md
@@ -0,0 +1,2 @@
+### 디자인패턴 강의 정리
+- [백기선님 유튜브 링크](https://www.youtube.com/watch?v=OwOEGhAo3pI&list=PLfI752FpVCS_v_sc8Q6V9QQN7GhoyktKD&index=1)
---
+++ src/main/java/s1/singleton/App.java
@@ -0,0 +1,19 @@
+package s1.singleton;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        /*
+        Settings set1 = new Settings();
+        Settings set2 = new Settings();
+        System.out.println(set1 != set2);
+        */
+
+        Settings set1 = Settings.getInstance();
+        Settings set2 = Settings.getInstance();
+        System.out.println(set1 == set2);
+
+    }
+
+}
